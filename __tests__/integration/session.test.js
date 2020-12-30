9723f3a33f89f5529edf8d8f0df61a7b63be6f46
David Carmo <davidsc2812@gmail.com>
2020-12-29 22:50:19 -0300

    Primeiros testes
    foram feitos os primeiros testes com o usuário relacionados a o login, comparando as senhas e usuário existente.

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,19 +1,55 @@
+const request = require('supertest'); 
+const app = require('../../src/app');
 const {User} = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
 
 describe('Authentication', ()=>
 { 
-it('should sum two numbers', async ()=>
+
+beforeEach(async () => 
+{
+  await truncate();
+})
+
+it('Should atuthenticate with valid credentials', async ()=>
 {
   const user = await User.create(
       {
           name: 'David', 
           email: 'david@david.com', 
-          password_hash: "123123"
+          password: "123123"
       }
   ); 
 
-  console.log(user); 
+  const response = await  request(app)
+  .post("/sessions")
+      .send({
+        email: user.email, 
+        password: "123123"
+      }); 
 
-  expect(user.email).toBe('david@david.com');
+  expect(response.status).toBe(200);
 });
-})
+
+it('Should not atuthenticate with valid credentials', async ()=>
+{
+  const user = await User.create(
+      {
+          name: 'David', 
+          email: 'david@david.com', 
+          password: "123123"
+      }
+  ); 
+
+  const response = await request(app)
+  .post("/sessions")
+      .send({
+        email: user.email, 
+        password: "123456"
+      }); 
+
+  expect(response.status).toBe(401);
+});
+
+});
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,11 +1,33 @@
+const bcrypt = require('bcryptjs'); 
 module.exports = (sequelize, DataTypes) => 
 {
     const User = sequelize.define("User", 
     {
         name: DataTypes.STRING, 
         email: DataTypes.STRING, 
+        password: DataTypes.VIRTUAL,
         password_hash: DataTypes.STRING,
-    }); 
+    },
+    {
+        hooks: 
+        {
+            beforeSave: async user => 
+            {
+                console.log(user.password);
+                if(user.password)
+                {
+                    console.log('tá na funfancia');
+                    user.password_hash = await bcrypt.hash(user.password, 8);
+                }
+            }
+        }
+    }
+    ); 
 
+    User.prototype.checkPassword = function(password)
+    {
+
+        return bcrypt.compare(password, this.password_hash); 
+    }
     return User; 
 };
